45d166d858d78815865ff8b9d21faa1af7f0671f
Ильин Владимир <v_ilyin@inline.local>
2016-10-17 18:12:42 +0300

    0.3 Улучшена обработка xml, добавлена сверка с предыдущими данными JSON

--- vipnet_count/vipnet_count.py
+++ vipnet_count/vipnet_count.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
 Created on 13 окт. 2016 г.
@@ -10,9 +10,9 @@ import optparse
 import os
 import sys
 import datetime
+import xmltodict
 
-from xlwt import Workbook, easyxf, Formula
-from xml2json import xml2json
+from xlwt import Workbook, easyxf
 
 def vipnet_json2xls(data, out_file):
     """Write data to Excel spreadsheet."""
@@ -33,9 +33,7 @@ def vipnet_json2xls(data, out_file):
     sheet = book.add_sheet('Vipnet', cell_overwrite_ok=True)
 
     columns = ("name","type","product-version","drv-version","monitor-version","ifaces","ip-list","timestamp")
-    #print(columns)
     for i, row in enumerate(data):
-        #print(row)
         for j, col in enumerate(columns):
             if i == 0:
                 sheet.write(i, j, col, header_style)
@@ -46,14 +44,6 @@ def vipnet_json2xls(data, out_file):
                     sheet.write(i+1, j, '', cell_style)
             elif col in ["ifaces"]:
                 sheet.write(i+1, j, len(row[col]), cell_style)
-    #rowx = 1
-    #for device in data:
-    #    sheet.write(rowx, colx, value, header_style)
-
-    #current_datetime = datetime.datetime.now().strftime("%Y-%m-%d--%H-%M")
-    
-    #if not os.path.exists(os.path.join(os.getcwd(),"xls\\")):
-    #    os.makedirs(os.path.join(os.getcwd(),"xls\\"))
 
     sheet.write(0,9,"G1",header_style)
     sheet.write(1,9,"=СЧЁТЕСЛИ($F:$F;2)",cell_style)
@@ -68,39 +58,42 @@ def vipnet_json2xls(data, out_file):
         os.remove(out_file)
 
     try:
-        #book.save(os.path.join(os.getcwd(), "xls\\" + current_datetime + '.xls'))
         book.save(out_file)
     except Exception as e:
         print("Error: %s" % str(e))
     return
 
+def datetime_parser(dct):
+    for k, v in dct.items():
+        if k in "timestamp":
+            try:
+                dct[k] = datetime.datetime.strptime(v, "%Y-%m-%dT%H:%M:%S")
+            except:
+                pass
+    return dct
+
 def main():
     current_datetime = datetime.datetime.now().strftime("%Y-%m-%d--%H-%M")
-    
+
+    datehandler = lambda obj: (
+                               obj.isoformat()
+                               if isinstance(obj, datetime.datetime)
+                               or isinstance(obj, datetime.date)
+                               else None
+                               )
+
     p = optparse.OptionParser(
         description='Converts XML data from StateWatcher to XLS and JSON.  Reads from file.',
         prog='vipnet_count',
         usage='%prog -o OUT file.xml'
     )
     p.add_option('--out', '-o', help="Write to OUT (OUT.xls and OUT.json)", default=current_datetime)
-    #p.add_option('--in', '-i', help="Input json file with VipNet data")
-    p.add_option(
-        '--strip_text', action="store_true",
-        dest="strip_text", help="Strip text for xml2json")
-    p.add_option(
-        '--pretty', action="store_true",
-        dest="pretty", help="Format JSON output so it is easier to read")
-    p.add_option(
-        '--strip_namespace', action="store_true",
-        dest="strip_ns", help="Strip namespace for xml2json")
-    p.add_option(
-        '--strip_newlines', action="store_true",
-        dest="strip_nl", help="Strip newlines for xml2json")
+    p.add_option('--input-json', '-i', help="Input json file with VipNet data")
     options, arguments = p.parse_args()
 
-    print("load...")
     if len(arguments) == 1:
         try:
+            print("load...")
             xml_file = open(arguments[0])
         except:
             sys.stderr.write("Problem reading '{0}'\n".format(arguments[0]))
@@ -113,81 +106,67 @@ def main():
     xml_data = xml_file.read()
     xml_file.close()
         
-    strip = 0
-    strip_ns = 0
-    if options.strip_text:
-        strip = 1
-    if options.strip_ns:
-        strip_ns = 1
-    if options.strip_nl:
-        xml_data = xml_data.replace('\n', '').replace('\r','')
-    
     print("convert...")
-    data = xml2json(xml_data, options, strip_ns, strip)
-    #data = {"export": {"rt":[]}}
+    data = xmltodict.parse(xml_data)
     xml_data = ""
-    
-    #with open('data-01.json') as data_file:    
-    #    data = json.load(data_file)
 
-    print("process..")
-    #if (options.in):
-    #    try:
-    #        xml_file = open(arguments[0])
-    #    except:
-    #        sys.stderr.write("Problem reading '{0}'\n".format(arguments[0]))
-    #        p.print_help()
-    #        sys.exit(-1)
-    #else:
-    vipnet = []
+    print("process...")
     vipnet_count = 0
     vipnet_count_G1 = 0
     vipnet_count_G2 = 0
+    if (options.input_json):
+        try:
+            vipnet = json.load(open(options.input_json), object_hook=datetime_parser)
+            for n in vipnet:
+                vipnet_count = vipnet_count+1
+                if "ifaces" in n:
+                    if len(n["ifaces"]) == 2:
+                        vipnet_count_G1 = vipnet_count_G1+1
+                    if len(n["ifaces"]) == 4:
+                        vipnet_count_G2 = vipnet_count_G2+1
+        except:
+            sys.stderr.write("Problem reading '{0}'\n".format(options.input_json))
+            p.print_help()
+            sys.exit(-1)
+    else:
+        vipnet = []
     repl = 0
+    repl_renew = 0
     i = 0
-    #print("обработано записей: ", i, end="")
+    print("обработано записей: %d\r"%i, end="")
     for dev in data["export"]["rt"]:
         i = i+1
-        #print("\b"*len(str(i-1)), i, end="")
-        #print(json.dumps(dev, indent=4, sort_keys=True))
+        print("обработано записей: %d\r"%i, end="")
         
-        #if os.path.exists("text1.json"):
-        #    os.remove("text1.json")
-        #f = open("text1.json", "w")
-        #f.write(json.dumps(dev, indent=4, sort_keys=True))
-        #f.close()
-
-        
-        #break
         ifaces = []
         if "ifaces" in dev:
             for ff in dev["ifaces"]["iface"]:
                 new_ff = {
-                          "name": ff["iface-name"]["#text"],
-                          "ip": ff["iface-ip"]["#text"],
-                          "netmask": ff["iface-netmask"]["#text"]
+                          "name": ff["iface-name"],
+                          "ip": ff["iface-ip"],
+                          "netmask": ff["iface-netmask"]
                           }
                 ifaces.append(new_ff)
         version = ''
         if "product-version" in dev:
-            version = dev["product-version"]["#text"]
+            version = dev["product-version"]
         drv_ver = ''
         if "drv-version" in dev:
-            drv_ver = dev["drv-version"]["#text"]
+            drv_ver = dev["drv-version"]
         mon_ver = ''
         if "monitor-version" in dev:
-            mon_ver = dev["monitor-version"]["#text"]
+            mon_ver = dev["monitor-version"]
         
         timestamp = ''
         if "poll-timestamp" in dev:
-            timestamp = datetime.datetime.strptime( dev["poll-timestamp"]["#text"], "%Y-%m-%d %H:%M:%S")
+            timestamp = datetime.datetime.strptime( dev["poll-timestamp"], "%Y-%m-%d %H:%M:%S")
         ip_list = ''
         if "ip-list" in dev:
-            ip_list = dev["ip-list"]["#text"]
+            ip_list = dev["ip-list"]
 
         new = {
-                "name": dev["node-name"]["#text"],
-                "type": dev["node-type"]["#text"],
+                "name": dev["node-name"],
+                "type": dev["node-type"],
                 "ip-list": ip_list,
                 "ifaces": ifaces,
                 "product-version": version,
@@ -204,6 +183,15 @@ def main():
                     repl = repl+1
                     vipnet_count = vipnet_count-1
                     break
+                elif ("COORDINATOR" not in n["type"]) and ("COORDINATOR" not in new["type"]) and (n["timestamp"] < new["timestamp"]):
+                    if len(n["ifaces"]) == 2:
+                        vipnet_count_G1 = vipnet_count_G1-1
+                    if len(n["ifaces"]) == 4:
+                        vipnet_count_G2 = vipnet_count_G2-1
+                    del vipnet[j]
+                    repl_renew = repl_renew+1
+                    vipnet_count = vipnet_count-1
+                    break
                 else:
                     node_exist = True
         if not node_exist:
@@ -216,16 +204,9 @@ def main():
             
             vipnet.append(new)
 
-    datehandler = lambda obj: (
-                               obj.isoformat()
-                               if isinstance(obj, datetime.datetime)
-                               or isinstance(obj, datetime.date)
-                               else None
-                               )
-
-    #print(json.dumps(vipnet, indent=4, sort_keys=True, default=datehandler))
     print("обработано записей: ", i)
-    print("перезаписано записей: ", repl)
+    print("перезаписано записей с новым типом: ", repl)
+    print("перезаписано записей с новой датой: ", repl_renew)
     print("vipnet count G1 + G2 + недоступны: ", vipnet_count)
     print("vipnet G1 count: ", vipnet_count_G1)
     print("vipnet G2 count: ", vipnet_count_G2)
